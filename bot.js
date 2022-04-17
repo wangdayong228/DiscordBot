557b927f82c665abfa0122fe7fde7b1a44786ccc
dayong <wangdayong228@163.com>
2022-04-17 17:00:16 +0800

    support command

--- bot.js
+++ bot.js
@@ -1,15 +1,38 @@
-    const {Client, RichEmbed, Discord} = require('discord.js') ;
-    const client = new Client ;
-    const settings = require('./settings.json') ;
-    
-    client.on('ready',()=>{
-        console.log(`${client.user.tag} 準備好上戰場惹！`) ;
-    }) ;
+const { Client, RichEmbed, Discord } = require('discord.js');
+const ethers = require('ethers');
+const client = new Client;
+const settings = require('./settings.json');
 
-    client.on('message',msg=>{
-        if(msg.content.startsWith(settings.prefix+'test')){
-            msg.channel.send('test committed') ;
+client.on('ready', () => {
+    console.log(`${client.user.tag} 準備好上戰場惹！`);
+});
+
+client.on('message', async (msg) => {
+    if (msg.content.startsWith(settings.prefix + 'test')) {
+        msg.channel.send('test committed');
+    }
+    if (msg.content.startsWith(settings.prefix + 'faucet')) {
+        let addr = msg.content.substring((settings.prefix + 'faucet').length + 1);
+        if (!ethers.utils.isAddress(addr)) {
+            msg.channel.send('please input a valid address');
+            return
         }
-    }) ;
+        let txhash = await sendCFX(addr);
+        let link = `https://evm.confluxscan.net/tx/${txhash}`
+        msg.channel.send(`sent ${settings.faucet_amount} CFX to ${addr}, please check by ${link}`);
+    }
+});
 
-    client.login(settings.token) ;
+client.login(settings.token);
+
+
+async function sendCFX(addr) {
+    let provider = new ethers.providers.JsonRpcProvider(settings.rpc_url);
+    let wallet = new ethers.Wallet(settings.faucet_key, provider);
+    let tx = await wallet.sendTransaction({
+        to: addr,
+        value: ethers.utils.parseEther(settings.faucet_amount.toString())
+    });
+    console.log(tx);
+    return tx.hash
+}
